2e9b6f760629a3a8f488c982f13bde402fbb29fc
Ewerton Mendonça <netrometro@yahoo.com.br>
2023-07-29 17:35:43 -0300

    Trabalho 05 e Elemento do trabalho 04

--- src/br/mendonca/trabalho04/elementos/FulanoDeTalElemento.java
+++ src/br/mendonca/trabalho04/elementos/FulanoDeTalElemento.java
@@ -1,23 +1,6 @@
 package br.mendonca.trabalho04.elementos;
 
-import br.mendonca.trabalho04.ITAD;
-
 public class FulanoDeTalElemento {
-
-	private ITAD tad;
-	private FulanoDeTalElemento next;
 	
-	public ITAD getTad() {
-		return tad;
-	}
-	public void setTad(ITAD tad) {
-		this.tad = tad;
-	}
-	public FulanoDeTalElemento getNext() {
-		return next;
-	}
-	public void setNext(FulanoDeTalElemento next) {
-		this.next = next;
-	}
 	
 }
